e32709bb4da9017a514848e24e4303b1f937d4c0
Hamim Alam <82621331+alamhamim@users.noreply.github.com>
2023-05-12 23:35:06 -0400

    Function updated

--- src/main/java/com/qa/lib/InvokeBatFile.java
+++ src/main/java/com/qa/lib/InvokeBatFile.java
@@ -3,16 +3,32 @@ package com.qa.lib;
 import java.io.IOException;
 
 public class InvokeBatFile {
-    
     public InvokeBatFile(String filePath){
-        String sFilePath = GetCurrDir.currDir()+"/"+filePath;
-        try {
-            Runtime.getRuntime().exec(sFilePath);
-            Thread.sleep(5000);
-            System.out.println("Bat file ran successfully!!!!!!!");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        int xCode = executeBatchFile(filePath);
+        if (xCode <= 0) ThrowError.error();
+    }
+    private int executeBatchFile(String filePath) {
+        ProcessBuilder processBuilder = null;
+        Process process = null;
+        int exitCode = 0;
+        if (!OSUTIL.getOS().equalsIgnoreCase("windows")) {
+            ThrowError.error();
         }
+        try {
+            processBuilder = new ProcessBuilder("cmd.exe", "/c", GetCurrDir.currDir() + "/" + filePath);
+            process = processBuilder.start();
+            exitCode = process.waitFor();
+            System.out.println("Batch file executed with code: " + exitCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (process != null) {
+                process.destroy();
+            } else {
+                System.out.println("Already closed!!!");
+            }
+        }
+        return exitCode;
     }
 
 }
